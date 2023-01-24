b217e0c2b6c03e25ebb43b728ab1641f4b0c3224
nayanachethan <123446837+nayanachethan@users.noreply.github.com>
2023-01-24 13:34:04 +0530

    Add files via upload

---
+++ udemy-python/python_basics2/loops.py
@@ -0,0 +1,13 @@
+is_old = False
+is_licenced = True
+
+if is_old and is_licenced:
+    print('you are old enough to drive, and you have license!')
+
+elif  is_licenced:
+    print('you can drive now!')
+
+else:
+    print('you are not of age!')   
+
+print('okokok')
