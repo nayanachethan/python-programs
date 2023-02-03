96169c1fdf5bb6f0643451a1cd595954f0c24c20
nayanachethan <nkaranth001@gmail.com>
2023-02-03 19:57:21 +0530

    committing

---
+++ udemy-python/python_basics1/for_loops.py
@@ -0,0 +1,6 @@
+# for loops iterate the collection of items
+for item in 'zero to Mastery' :
+    print(item)
+    
+for item in {1,2,3,4}:
+      print(item)    
---
+++ udemy-python/python_basics2/for_loops.py
@@ -0,0 +1,27 @@
+# for loops iterate the collection of items
+# for item in (1,2,3,4,5) :
+ # for x in ['a' , 'b', 'c'] :
+    # print(item, x)
+    
+# Iterables - list, dictonary,tuple,set,string 
+# iterated - one by one check each item in the collection
+
+user =  {
+    'name': 'andrie',
+    'age': 56,
+    'can_swim':True 
+ }
+
+for item in user.items():
+     print(item)
+for item in user.values():
+     print(item)
+for item in user.keys():     
+    print(item)
+
+my_list = [1,2,3,4,5,6,7,8]
+
+counter= 0
+for item in my_list:
+  counter = counter + item
+print(counter)
