1a2d7a13e62bf63c94bf3ca3c0aa8a1147e52dce
nayanachethan <nkaranth001@gmail.com>
2023-02-06 20:30:29 +0530

    updated

--- udemy-python/python_basics2/range.py
+++ udemy-python/python_basics2/range.py
@@ -10,26 +10,3 @@
 
 #         print(f'index of 50 is:{i}')
 
-# While loops
-# i = 1
-# while i < 50:
-#      print(i)
-#      i  += 1
-#      break
-# else: 
-#         print('done with all work')
-
-# my_list= [1,2,3]
-# for item in my_list:
-#     print(item)
-
-# i = 0
-# while i <len(my_list):
-#     print(my_list[i])
-#     i += 1
-
-while True:
-    response = input('say something:')
-    if (response == 'bye'):
-      break
-   
---
+++ udemy-python/python_basics2/while_loops.py
@@ -0,0 +1,35 @@
+# While loops
+# i = 1
+# while i < 50:
+#      print(i)
+#      i  += 1
+#      break
+# else: 
+#         print('done with all work')
+
+# my_list= [1,2,3]
+# for item in my_list:
+#     print(item)
+
+# i = 0
+# while i <len(my_list):
+#     print(my_list[i])
+#     i += 1
+
+# while True:
+#     response = input('say something:')
+#     if (response == 'bye'):
+#       break
+   
+my_list= [1,2,3]
+for item in my_list:
+    print(item)
+    continue
+
+i = 0
+while i <len(my_list):
+    print(my_list[i])
+    i += 1
+    continue
+
+#pass - passes the line 
